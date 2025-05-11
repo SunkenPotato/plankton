2c4f3cd8831f2ae49ebc849defa9e5e12c383c03
SunkenPotato <3.14radius02@gmail.com>
2025-05-11 17:22:10 +0200

    setup main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::{
+    DefaultPlugins,
+    app::App,
+    log::{info, warn},
+};
+use bevy_ecs_ldtk::LdtkPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+pub static IS_DEBUG: bool = cfg!(debug_assertions);
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins).add_plugins(LdtkPlugin);
+
+    if IS_DEBUG {
+        warn!("This game was built for debug!");
+        warn!("If you are not an end-user, it is highly recommended to use a stable release.");
+
+        app.add_plugins(WorldInspectorPlugin::new());
+    }
+
+    info!("Exiting");
 }
